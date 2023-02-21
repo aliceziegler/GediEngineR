919387912c352087a8f2f4f7f6fa77a860ad661d
Ziegler Alice <ziegler5@gpunode31.localdomain>
2023-02-21 11:39:52 +0100

    minor changes, delete marker

--- scripts/100_R_plot_spatial_preds.R
+++ scripts/100_R_plot_spatial_preds.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Description: plot spatial predictions from GEE
 # Author: Alice Ziegler
 # Date:
@@ -184,6 +183,97 @@ map_plot <-
   # ggtitle(paste0("month_", i)) +
   ggtitle(month.name[i]) +
   geom_sf(data = hessen, fill = NA, color = "red")+
+  # # geom_polygon(mapping = aes(x = long, y = lat, group = group),data = overlay_df, color = "red", fill = NA)+
+  # geom_polygon(mapping = aes(x = long, y = lat, group = group),data = exmpl_df, color = "black", fill = NA, size = 0.4)+ #(0.1)
+  # geom_text(mapping = aes(x = X, y = Y, label = area), data = pred_exmpl_labels, colour = "black", fontface = "bold", size = 4)+ #size = 1.8,
+  theme(panel.background = element_blank(), #panel.grid = element_line(color = "grey80"),
+        axis.text.y = element_text(angle = 90, hjust = 0.5, colour = "black", size = 6),
+        axis.text.x = element_text(colour = "black", size = 6),
+        axis.title.x = element_text(size = 6),
+        axis.title.y = element_text(size = 6),
+        plot.title = element_text(size = 10, face = "bold"), #margin = margin(-0.5,0,0,0),
+        legend.text = element_text(size = 6),
+        legend.title = element_text(size = 10),
+        legend.key.size = unit(0.5, "cm"),
+        axis.ticks = element_line(size = 0.02),
+        axis.ticks.length = unit(0.05, "cm"),
+        plot.margin = unit(c(0.4 , -1.2,-0.6,-1.0), "cm"))
+print(map_plot)
+  ggsave(filename = paste0(fig_path, "100_map_prediction_month_", i, "_", comm_mod, "_", comm_comp, ".pdf"),
+  plot = map_plot,
+  width = 200, height = 150, units = "mm", dpi = 300)
+
+  ggsave(filename = paste0(fig_path, "100_map_prediction_month_", i, "_", comm_mod, "_", comm_comp, ".png"),
+         plot = map_plot,
+         width = 200, height = 150, units = "mm", dpi = 300)
+
+return(map_plot)
+})
+
+# #für marvin
+# plot_list_sml <- plot_list[c(3,5,7,9)]
+# saveRDS(plot_list_sml, file = paste0(proj_path, "div/ggarrange_TS.rds"))
+# ggpubr::ggarrange(plotlist = plot_list_sml, ncol= 4, nrow = 1, # widths = (1,1,1,1), #heights = c(1,1,1,0.2),
+#                   common.legend = T, legend = c("bottom", "left"))
+# ###
+
+# plot_grid <-
+  ggpubr::ggarrange(plotlist = plot_list[c(3,5,7,9)], ncol= 4, nrow = 1, # widths = (1,1,1,1), #heights = c(1,1,1,0.2),
+                    common.legend = T, legend = c("bottom", "left"))
+                   # ,        font.label = list(size = 3))
+
+ggsave(filename = paste0(fig_path, "100_plot_grid_3_5_7_9", "_", comm_mod, "_", comm_comp, ".pdf"),
+       plot = plot_grid,
+       width = 200, height = 150, units = "mm", dpi = 300)
+
+ggsave(filename = paste0(fig_path, "100_plot_grid_3_5_7_9", "_", comm_mod, "_", comm_comp, ".png"),
+       plot = plot_grid,
+       width = 200, height = 150, units = "mm", dpi = 300)
+
+
+### animation
+
+animation::saveGIF(
+  expr = {
+    plot(plot_list[[1]])
+    plot(plot_list[[2]])
+    plot(plot_list[[3]])
+    plot(plot_list[[4]])
+    plot(plot_list[[5]])
+    plot(plot_list[[6]])
+    plot(plot_list[[7]])
+    plot(plot_list[[8]])
+    plot(plot_list[[9]])
+    plot(plot_list[[10]])
+    plot(plot_list[[11]])
+    plot(plot_list[[12]])
+    },
+  movie.name = paste0("prediction_", comm_comp, ".gif")
+)
+
+#####
+### plotting predictions
+#####
+plot_list <- lapply(seq(lst_preds), function(i){
+
+rst <- raster(lst_preds[i])
+
+map_plot <-
+  ggR(rst, geom_raster = TRUE) +
+  # scale_fill_gradientn(name = "PAI", colors = LAI_col(25))+
+  # scale_fill_gradientn(name = "PAI", colors = scico_pal(100), na.value = "white")+
+    scale_fill_viridis(limits = c(0, 3.5), oob = scales::squish)+
+  # scale_fill_scico(palette = "batlow", aesthetics = "colour", alpha = NULL,
+  # begin = 0,
+  # end = 1,
+  # direction = 1)+
+  # scale_fill_material(name = "PAI", "green")+
+  # scale_fill_viridis(name = "PAI", option="inferno")+
+  # scale_x_continuous(name = "Longitude", expand = c(0,0), breaks = breaks_x, labels = labels_x)+
+  # scale_y_continuous(name = "Latitude", expand = c(0,0), breaks = breaks_y, labels = labels_y)+#n.breaks = 3)+
+  # ggtitle(paste0("month_", i)) +
+  ggtitle(month.name[i]) +
+  geom_sf(data = hessen, fill = NA, color = "red")+
   labs(fill = "PAI")+
   scale_x_continuous(expand = c(0, 0)) +
   scale_y_continuous(expand = c(0, 0)) +
@@ -262,5 +352,3 @@ animation::saveGIF(
     },
   movie.name = paste0("prediction_", comm_comp, ".gif")
 )
-
->>>>>>> 5d7c4179c75b24c850ab23108580e6ed64610e4c
